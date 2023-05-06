20b9092d2af82b18ed6ec269ec526bc53054b12a
Nur-Otan <107070626+Nur-Otan@users.noreply.github.com>
2023-05-06 10:30:17 +0500

    first-commit

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,7 +3,6 @@ import { Button, Card } from 'react-bootstrap';
 import { useThemeHook } from '../GlobalComponents/ThemeProvider';
 import { useCart } from 'react-use-cart';
 import { BsCartPlus } from 'react-icons/bs';
-import { Link } from "@reach/router";
  
 const ProductCard = (props) => {
     let { image, price, title } = props.data;
